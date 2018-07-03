39cdcc39c9cbe985a79e3751cf56993256825e00
cherucole <cheruyotcheruyot@gmail.com>
2018-07-03 17:06:07 +0300

    modified js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
-var add = function(number1, number2) {return number1 + number2;};
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var multiply=function(number1, number2) {
+  return number1 * number2;
+};
+
+var division=function(number1, number2) {
+  return number1 / number2;
+};
 
 
-var number1= parseInt(prompt("Enter a number"));
-var number2=parseInt(prompt("enter another number"));
-alert(add(number1, number2));
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("enter another number"));
+var result = multiply(number1, number2);
+alert(result);
